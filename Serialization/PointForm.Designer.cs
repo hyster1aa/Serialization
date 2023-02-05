27c099831b7f4629b7e3c9831c4b8b5db63340dd
mixai <mixai@DESKTOP-6DKU1B0>
2023-02-05 11:51:12 +0300

    Добавьте файлы проекта.

---
+++ Serialization/PointForm.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Serialization
+{
+    partial class PointForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.btnSort = new System.Windows.Forms.Button();
+            this.btnSerialize = new System.Windows.Forms.Button();
+            this.btnDeserialize = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(776, 238);
+            this.listBox1.TabIndex = 0;
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(102, 256);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(116, 41);
+            this.btnCreate.TabIndex = 1;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // btnSort
+            // 
+            this.btnSort.Location = new System.Drawing.Point(254, 256);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(116, 41);
+            this.btnSort.TabIndex = 2;
+            this.btnSort.Text = "Sort";
+            this.btnSort.UseVisualStyleBackColor = true;
+            this.btnSort.Click += new System.EventHandler(this.btnSort_Click);
+            // 
+            // btnSerialize
+            // 
+            this.btnSerialize.Location = new System.Drawing.Point(409, 256);
+            this.btnSerialize.Name = "btnSerialize";
+            this.btnSerialize.Size = new System.Drawing.Size(116, 41);
+            this.btnSerialize.TabIndex = 3;
+            this.btnSerialize.Text = "Serialize";
+            this.btnSerialize.UseVisualStyleBackColor = true;
+            this.btnSerialize.Click += new System.EventHandler(this.btnSerialize_Click);
+            // 
+            // btnDeserialize
+            // 
+            this.btnDeserialize.Location = new System.Drawing.Point(564, 256);
+            this.btnDeserialize.Name = "btnDeserialize";
+            this.btnDeserialize.Size = new System.Drawing.Size(116, 41);
+            this.btnDeserialize.TabIndex = 4;
+            this.btnDeserialize.Text = "Deserialize";
+            this.btnDeserialize.UseVisualStyleBackColor = true;
+            this.btnDeserialize.Click += new System.EventHandler(this.btnDeserialize_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "SOAP|*.soap|XML|*.xml|JSON|*.json|Binary|*.bin";
+            // 
+            // PointForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 366);
+            this.Controls.Add(this.btnDeserialize);
+            this.Controls.Add(this.btnSerialize);
+            this.Controls.Add(this.btnSort);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.listBox1);
+            this.Name = "PointForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Button btnSort;
+        private System.Windows.Forms.Button btnSerialize;
+        private System.Windows.Forms.Button btnDeserialize;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Serialization/PointForm.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization.Formatters.Soap;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+using PointLib;
+using Newtonsoft.Json;
+
+
+namespace Serialization
+{
+
+    public partial class PointForm : Form
+    {
+
+        public PointForm()
+        {
+            InitializeComponent();
+        }
+        [Serializable]
+        public class Point3D : Point
+        {
+            public int Z { get; set; }
+
+            public Point3D() : base()
+            {
+                Z = rnd.Next(10);
+            }
+
+            public Point3D(int x, int y, int z) : base(x, y)
+            {
+                Z = z;
+            }
+
+            public override double Metric()
+            {
+                return Math.Sqrt(X * X + Y * Y + Z * Z);
+            }
+
+            public override string ToString()
+            {
+                return string.Format($"({X} , {Y}, {Z})");
+            }
+        }
+        private Point[] points = null;
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            points = new Point[5];
+
+            var rnd = new Random();
+
+            for (int i = 0; i < points.Length; i++)
+                points[i] = rnd.Next(3) % 2 == 0 ? new Point() : new Point3D();
+
+            listBox1.DataSource = points;
+        }
+
+        private void btnSort_Click(object sender, EventArgs e)
+        {
+            if (points == null)
+                return;
+
+            Array.Sort(points);
+
+            listBox1.DataSource = null;
+            listBox1.DataSource = points;
+
+        }
+
+        private void btnSerialize_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "SOAP|*.soap|XML|*.xml|JSON|*.json|Binary|*.bin";
+            if (saveFileDialog1.ShowDialog() != DialogResult.OK)
+                return;
+            using (var fs = new FileStream(saveFileDialog1.FileName, FileMode.Create, FileAccess.Write))
+            {
+                switch (Path.GetExtension(saveFileDialog1.FileName))
+                {
+                    case ".bin":
+                        var bf = new BinaryFormatter();
+                        bf.Serialize(fs, points);
+                        break;
+                    case ".soap":
+                        var sf = new SoapFormatter();
+                        sf.Serialize(fs, points);
+                        break;
+                    case ".xml":
+                        var xf = new XmlSerializer(typeof(Point[]),new[] {typeof(Point3D)});
+                        xf.Serialize(fs, points);
+                        break;
+                    case ".json":
+                        var jf = Newtonsoft.Json.JsonConvert.SerializeObject(points);
+                        var temp = Encoding.Default.GetBytes(jf);
+                        fs.Write(temp,0,temp.Count());
+                        break;
+                }
+            }
+        }
+
+        private void btnDeserialize_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() != DialogResult.OK)
+                return;
+            using (var fs = new FileStream(openFileDialog1.FileName, FileMode.Open, FileAccess.Read))
+            {
+                switch(Path.GetExtension(openFileDialog1.FileName))
+                {
+                    case ".bin":
+                        var bf = new BinaryFormatter();
+                        points = (Point[])bf.Deserialize(fs);
+                        break;
+                    case ".soap":
+                        var sf = new SoapFormatter();
+                        points = (Point[])sf.Deserialize(fs);
+                        break;
+                    case ".xml":
+                        var xf = new XmlSerializer(typeof(Point[]), new[] {typeof(Point3D) });
+                        points = (Point[])xf.Deserialize(fs);
+                        break;
+                    case ".json":
+                        var jf = new JsonSerializer();
+                        using (var r = new StreamReader(fs))
+                            points = (Point[])jf.Deserialize(r, typeof(Point[]));
+                        break;
+
+                }
+            }
+            listBox1.DataSource = null;
+            listBox1.DataSource = points;
+
+        }
+    }
+    
+
+}
